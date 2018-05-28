073e2a06d856b4efe38c2aeaa3491e778ba29cbf
Jennifer Parra <jparra.biblio@gmail.com>
2018-05-28 00:09:36 -0400

    Reto - edades y etapas

---
+++ app.js
@@ -0,0 +1,26 @@
+
+var age = prompt("¿Cuál es tu edad?");
+
+if (age >=1 && age <= 2){
+    alert("Stage: toodler");
+}
+else if (age >= 3 && age <= 4){
+    alert("Stage: preschooler");
+}
+else if (age >= 5 && age <= 11){
+    alert("Stage: gradeschooler");
+}
+else if (age >= 12 && age <= 17){
+    alert("Stage: teenager");
+}
+else if (age >= 18 && age <= 21){
+    alert("Stage: young adult");
+}
+else if (age > 21){
+    alert("You are an adult!");
+}
+else    
+    alert("¡Error mortal! no puedes ingresar letras, números decimales ni dejar espacios vacios");
+
+
+  
